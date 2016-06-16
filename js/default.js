b7e0ecfac0d426c3729c49dde724b33a305755ad
Takashi Imagire <imagire@gmail.com>
2016-06-16 17:04:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,17 @@
-// 確認ダイアログを表示
-var result = window.confirm('準備OK?');
-// 結果を表示
-if(result){
-  document.getElementById('choice').textContent = 'OKが押されました';
+// 答えを用意
+var number = Math.floor(Math.random()*4);
+var answer = parseInt(window.prompt('数あてゲーム。0～3の数字を入力してください！'));
+
+var message;
+if(answer === number) {
+  message = 'あたり！';
+}else if (answer < number){
+  message = '残念！もっと大きいです';
+}else if (number < answer){
+  message = '残念！もっと小さいです';
 }else{
-  document.getElementById('choice').textContent = 'キャンセルが押されました';
+  message = '0～3の数字を入力してください';
 }
+
+// 結果を表示
+document.getElementById('choice').textContent = message;

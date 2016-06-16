8ae0fdc1b241c05e7bc61be24fe3a6f4a29911bf
Takashi Imagire <imagire@gmail.com>
2016-06-16 15:35:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,4 @@
-// 日時を表示
-document.getElementById('choice').textContent = new Date();
+// 確認ダイアログを表示
+var result = window.confirm('準備OK?');
+// 結果を表示
+document.getElementById('choice').textContent = result;

76612c12c03b90ff9f4d21ea2239c55cf7278b58
Takashi Imagire <imagire@gmail.com>
2016-06-16 15:44:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,8 @@
 // 確認ダイアログを表示
 var result = window.confirm('準備OK?');
 // 結果を表示
-document.getElementById('choice').textContent = result;
+if(result){
+  document.getElementById('choice').textContent = 'OKが押されました';
+}else{
+  document.getElementById('choice').textContent = 'キャンセルが押されました;
+}
